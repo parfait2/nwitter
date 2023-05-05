9340bc3afb3c246e2a34ef9945f6ea9a942c06d3
parfait2 <parkyl0208@naver.com>
2023-05-05 20:57:00 +0900

    React + Firebase + Router Setup

---
+++ src/components/Router.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+
+export default () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+        <Router>
+            <Switch>
+            </Switch>
+        </Router>
+    )
+}
